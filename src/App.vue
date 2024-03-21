198f4f62476209c578a9b5ab42746ee632d382ad
caiweiqiang <1429388864@qq.com>
2024-03-21 22:51:46 +0800

    feat: 封装axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
-
-
 <template>
-  <div>test</div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped></style>
+<style scoped>
+#app {
+  background: #eee;
+  height: 100vh;
+  overflow: hidden;
+  font-weight: 400 !important;
+}
+</style>
